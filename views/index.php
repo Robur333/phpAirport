3ef3a5748a1135d5b253b0caca90e01878a45b3a
Robert Spaleniak <robciopl25@gmail.com>
2023-01-26 18:42:45 +0100

    css update

--- views/index.php
+++ views/index.php
@@ -16,12 +16,12 @@
   </head>
   <body>
       <div class="pop-up hidden" id="pop-up-id">
-        <button class="close-tag grey-btn" onclick="closePopUp()">Return</button>
         <p>To reserve a flight, please <b>sign up</b> or <b>login</b> first</p>
         <div class=pop-up-buttons>
           <a class="navLink" href="/Login">Login</a>
           <a class="navLink" href="/register">Sign up</a>
         </div>
+        <button class="close-tag grey-btn" onclick="closePopUp()">Return</button>
       </div>
 
     <div class="container">
